ea729a79ddb97c897bb2299c8c1f7ea546fd05e4
stephaniaguzman <stephania.guzman@pucp.edu.pe>
2022-04-12 20:05:49 -0500

    Controllers

---
+++ src/main/java/com/example/lab2/Controllers/InventarioControllers.java
@@ -0,0 +1,10 @@
+package com.example.lab2.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inventario")
+
+public class InventarioControllers {
+}
---
+++ src/main/java/com/example/lab2/Controllers/SedeController.java
@@ -0,0 +1,17 @@
+package com.example.lab2.Controllers;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/sede")
+
+public class SedeController {
+
+}
