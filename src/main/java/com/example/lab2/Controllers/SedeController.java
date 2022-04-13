229170c3313ddce69c2bc6ac2436dddc8163e244
stephaniaguzman <stephania.guzman@pucp.edu.pe>
2022-04-12 21:57:55 -0500

    Correccion

--- src/main/java/com/example/lab2/Controllers/SedeController.java
+++ src/main/java/com/example/lab2/Controllers/SedeController.java
@@ -2,8 +2,16 @@ package com.example.lab2.Controllers;
 
 
 
+import com.example.lab2.Entity.Sedes;
+import com.example.lab2.repository.SedesRepository;
+import com.example.lab2.repository.TrabajadoresRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 import java.util.List;
